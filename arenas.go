906d5a6c5171725613de17cd74b5e7087a74e7f9
ranked <>
2022-02-24 20:13:48 +0000

    Add arenas file

---
+++ arenas.go
@@ -0,0 +1,10 @@
+package als_golang
+
+type ArenasReturnables struct {
+		
+}
+
+/* Function used to get the general statistics for a player from Arenas: kills, damage done, wins. */
+func ArenasGeneralStats(APIToken string) {
+		
+}
