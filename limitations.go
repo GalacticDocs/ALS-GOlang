6f91edb7019a51a27aef96dbf11071fcd47b1b46
Vitaliya <42318815+iVitaliya@users.noreply.github.com>
2022-03-01 23:47:53 +0100

    Adds

--- limitations.go
+++ limitations.go
@@ -1,27 +1,37 @@
 package als_golang
 
-var possible_platforms = []string{
-	"PC",
-	"X1",
-	"PS4",
-}
+import "errors"
+
+var (
+	possible_platforms = []string{"PC", "X1", "PS4"}
+)
 
 type PlatformChecker struct {
 	status        bool
 	platform_type string
 }
 
-func CheckPlatformArray(array []string, value string) *PlatformChecker {
-	var checker *PlatformChecker
+func CheckPlatformArray(array []string, value string) (*PlatformChecker, error) {
+	var (
+		checker *PlatformChecker = &PlatformChecker{
+			status:        false,
+			platform_type: "unknown",
+		}
+		InvPlatfErr = errors.New("The value \"" + value + "\" is not a valid platform, the platform must be \"PC\", \"X1\" or \"PS4\".")
+	)
 
-	for i := 0; i <= 2; i++ {
-		if value == array[i] {
+	for i := 0; i <= len(array); i++ {
+		if array[i] == value {
 			checker = &PlatformChecker{
-				status: true,
-				platform_type: array[i],
+				status:        true,
+				platform_type: value,
 			}
 		}
 	}
 
-	return checker
+	if checker.status == false || checker.platform_type == "unknown" {
+		return nil, InvPlatfErr
+	}
+
+	return checker, nil
 }
